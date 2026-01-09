8184336647a7b8c8a2438dfee3a74689c5aea4e8
Mitali Umesh Nilapwar <mitaliunilapwar@gmail.com>
2026-01-09 21:55:52 +0530

    Add Assignment 3

---
+++ Assignment_3/Qn_6.py
@@ -0,0 +1,12 @@
+from sys import getsizeof
+
+
+x = input("Enter anything :")
+
+print("Data type is : ", type(x))
+print("Memory Address is : " , id(x))
+print("size of x is : " , getsizeof(x))
+
+
+
+

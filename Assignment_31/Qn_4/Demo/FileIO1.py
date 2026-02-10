ed2b33e66d072cba409bf5f4cadd7efb658e11ce
Mitali Umesh Nilapwar <mitaliunilapwar@gmail.com>
2026-02-10 21:13:05 +0530

    Assignment_31/Qn_4

---
+++ Assignment_31/Qn_4/Demo/FileIO1.py
@@ -0,0 +1,7 @@
+#error code
+
+def main():
+    open("Demo.txt")   #Default is reading
+
+if __name__ == "__main__":
+    main()
---
+++ Assignment_31/Qn_4/Qn_4.py
@@ -0,0 +1,84 @@
+'''Design automation script which accept two directory names and one file extension. Copy all
+files with the specified extension from first directory into second directory. Second directory
+should be created at run time.
+
+Usage : DirectoryCopyExt.py "Demo" "Temp" ".exe"
+Demo is name of directory which is existing and contains files in it. We have to create new
+Directory as Temp and copy all files with extension .exe from Demo to Temp.'''
+
+
+import os
+import shutil
+import sys
+import time
+
+def DirectoryCopy(SourceDir, DestDir, Extension):
+    Border = "-"*50
+    timestamp = time.ctime()
+
+    logfilename = "Demo_%s.log" %(timestamp)
+    logfilename = logfilename.replace(" ","_")
+    logfilename = logfilename.replace(":","_")
+
+    fobj = open(logfilename,"w")
+    fobj.write(Border + "\n")
+    fobj.write("This is a log file created by Mitali\n")
+    fobj.write("This script Copies all files with the specified extension from first directory into second directory\n")
+    fobj.write(Border+"\n")
+
+    Ret = False
+    Ret =os.path.exists(SourceDir)
+
+    if(Ret == False):
+        print("There is no source directory\n")
+        return
+    
+    Ret = os.path.isdir(SourceDir)
+
+    if(Ret == False):
+        print("Source path is not a directory\n")
+        return
+    
+    if not os.path.exists(DestDir):
+        os.mkdir(DestDir)
+        fobj.write("Destination Directory created\n")
+
+    FileCount = 0
+
+    for FolderName,SubFolderName,FileName in os.walk(SourceDir):
+        for fname in FileName:
+                Source_path = os.path.join(FolderName,fname)
+                Dest_Path= os.path.join(DestDir,fname)
+                if fname.endswith(Extension):
+                    if os.path.exists(Dest_Path):
+                        fobj.write(f"Skipped (Already exists) : {Dest_Path} \n")
+                    else:
+                        shutil.copy(Source_path,Dest_Path)
+                        FileCount+=1
+                        fobj.write(f"Copied : {Source_path} to - {Dest_Path} \n")
+
+    fobj.write(Border + "\n")
+    fobj.write(f"Total files Copied : {FileCount} \n")
+    fobj.write(f"Log file created at :  {timestamp}\n")
+    fobj.write(Border + "\n")
+
+    
+    fobj.write(Border + "\n")
+    fobj.write("-------------------- Thank you -------------------" + "\n")
+
+    fobj.close()
+
+
+def main():
+
+    if len(sys.argv)!=4:
+        print("Invalid number of arguments")
+        print("Usage : DirectoryCopy.py SourceDir DestDir")
+
+        return
+    
+    DirectoryCopy(sys.argv[1],sys.argv[2],sys.argv[3])
+
+
+if __name__ == "__main__":
+    main()
